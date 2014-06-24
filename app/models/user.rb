d572c93c90187e109b412bc8ea7351a77a294ff2
kanhaiya007 <kanhaiya@joshsoftware.com>
2014-06-24 15:01:01 +0530

    Done with Relations and models with model spec
    Tested the relations also.

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User
+  include Mongoid::Document
+  include Mongoid::MagicCounterCache
+
+  field :email_id,      type: String
+  field :is_subscribed, type: Boolean
+  field :joined_on,     type: Date
+  field :source,        type: String
+
+  validates :email_id, presence: true
+  validates :is_subscribed, presence: true
+  validates :joined_on, presence: true
+  validates :source, presence: true
+  validates :email_id, uniqueness: true
+
+  belongs_to :newsletter
+  counter_cache :newsletter
+
+end
