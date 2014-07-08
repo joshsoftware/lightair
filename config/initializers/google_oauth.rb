c90053c8167cca306d417d23802884509591ad4e
basnal <pankajbasnal17@gmail.com>
2014-07-08 15:24:09 +0530

    Integrated Google Oauth in separate branch

--- config/initializers/google_oauth.rb
+++ config/initializers/google_oauth.rb
@@ -2,8 +2,10 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['GOOGLE_ID'], ENV['GOOGLE_KEY'],
   {
     name:                 "google",
-    scope:                "userinfo.profile,userinfo.email,drive,https://spreadsheets.google.com/feeds",
-    prompt:               "consent",
+    scope:                'userinfo.profile,userinfo.email,drive,https://spreadsheets.google.com/feeds',
+    prompt:               'consent',
     access_type:          "offline",
   }
+
+  #provider :linkedin, '75cmlzl0cpmwa2', 'I5y0aRBkfpgMmTqp', redirect_uri: 'http://localhost:8080/auth/linkedin/callback', scope:'r_emailaddress r_network r_contactinfo rw_company_admin rw_nus rw_groups w_messages r_basicprofile r_fullprofile'
 end
