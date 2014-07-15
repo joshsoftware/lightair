cb502c2756198289642eaceec469eb26b7fefdaf
basnal <pankajbasnal17@gmail.com>
2014-07-15 10:21:15 +0530

    Unnecessary files removed #11

--- app/models/token.rb
+++
@@ -1,8 +0,0 @@
-class Token
-  include Mongoid::Document
-
-  field :spreadsheet_id, type: String
-  field :refresh_token,  type: String
-  field :expires_at,     type: Time
-  field :access_token,   type: String
-end
