0bfed4ef942ebed612b65a5ba21178bf87573d49
Anuja Ware <anuja@joshsoftware.com>
2014-08-28 18:01:47 +0530

    Added sendgrid settings

--- config/initializers/google_oauth.rb
+++ config/initializers/google_oauth.rb
@@ -11,3 +11,15 @@ end
 OmniAuth.config.on_failure = Proc.new { |env|
   OmniAuth::FailureEndpoint.new(env).redirect_to_failure
 }
+
+common_settings = {
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
+Light::UserMailer.smtp_settings = common_settings.merge({
+  :user_name => ENV['NEWSLETTER_SENDGRID_USERNAME'],
+  :password => ENV['NEWSLETTER_SENDGRID_PASSWORD'],
+  :address => "smtp.sendgrid.net",
+})
