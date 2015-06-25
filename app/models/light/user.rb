d490155c620f68997fad52e48d45767db0a444fb
Anuja Ware <anuja@joshsoftware.com>
2015-06-25 11:58:09 +0530

    Ref #76 Html page updated for Import contacts

--- app/models/light/user.rb
+++ app/models/light/user.rb
@@ -38,6 +38,7 @@ module Light
     end
 
     def self.import(file)
+      return {error: "Please select file"} unless file
       rows = CSV.read(file.path)
       header = rows.delete_at(0)
       return {error: "Header doesn't matches"} if header != ['Full Name', 'Email']
