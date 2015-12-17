3ef562d1a29fa4266e85490d33ef41782c31a4c8
Shweta <shweta@joshsoftware.com>
2015-12-17 21:00:52 +0530

    Avoid duplication while sending newsletter.

--- app/workers/light/hard_worker.rb
+++ app/workers/light/hard_worker.rb
@@ -6,9 +6,11 @@ module Light
     def perform(user_ids, newsletter_id, date)
       newsletter = Light::Newsletter.where(id: newsletter_id).first
       user_ids.each do |id| 
-        user = Light::User.where(id: id).first
-        Light::UserMailer.welcome_message(user.email_id, newsletter, user.slug).deliver
-        user.update_attribute :sent_on, user.sent_on << date
+        user = Light::User.where(id: id, is_subscribed: true, :sent_on.nin => [date]).first
+        if user.present?
+          Light::UserMailer.welcome_message(user.email_id, newsletter, user.slug).deliver
+          user.update_attribute :sent_on, user.sent_on << date
+        end
       end
     end
   end
