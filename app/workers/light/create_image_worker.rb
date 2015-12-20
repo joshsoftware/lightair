f8466460bb7e4e67b7ca93ecfe1c03355109505c
Shweta <shweta@joshsoftware.com>
2015-12-20 15:56:09 +0530

    Updated constant name(HOST_URL) as it was giving warning in intranet.
    Screamout has same constant

--- app/workers/light/create_image_worker.rb
+++ app/workers/light/create_image_worker.rb
@@ -6,7 +6,7 @@ module Light
 
     def perform(newsletter_id)
       newsletter = Light::Newsletter.where(id: newsletter_id).first
-      kit = IMGKit.new("#{HOST_URL}/web-version/#{newsletter.slug}",height: 600)
+      kit = IMGKit.new("#{LIGHT_HOST_URL}/web-version/#{newsletter.slug}",height: 600)
       img = kit.to_img(:jpg)
       file = kit.to_file("#{Rails.root}/public/newsletter/" + newsletter.slug)
       newsletter.update_attributes(photo: open("#{Rails.root}/public/newsletter/" + newsletter.slug))
