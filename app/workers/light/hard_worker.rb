1dcf2ce9d940b92077c6f94053bac8ca5ba5bee4
Swapnil Chincholkar <swapnil@joshsoftware.com>
2020-07-02 18:05:08 +0530

    fixed issue in newsletter send nullifying status

--- app/workers/light/hard_worker.rb
+++ app/workers/light/hard_worker.rb
@@ -5,7 +5,7 @@ module Light
 
     def perform(user_ids, newsletter_id, date, status = nil)
       newsletter = Light::Newsletter.where(id: newsletter_id).first
-      user_ids.each do |id| 
+      user_ids.each do |id|
         user = Light::User.where(id: id, :sent_on.nin => [date]).first
         if user.present?
           Light::UserMailer.welcome_message(user.email_id, newsletter, user.token).deliver
@@ -15,12 +15,12 @@ module Light
                                    sidekiq_status: status,
                                    opt_in_mail_sent_at: DateTime.now)
           elsif status.present? && status.include?('Opt out')
-            user.update_attributes(sent_on: sent_on, 
+            user.update_attributes(sent_on: sent_on,
                                    sidekiq_status: 'Subscribed',
                                    subscribed_at: DateTime.now,
                                    is_subscribed: true)
           else
-            user.update_attributes(sent_on: sent_on, sidekiq_status: status)
+            user.update_attributes(sent_on: sent_on)
           end
         end
       end
